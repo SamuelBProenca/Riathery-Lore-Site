7cef6ad2a0288a01fcbb4082b824083ef9a701ea
samuel <samuelbrito782@gmail.com>
2023-08-16 12:56:37 -0300

    adiciona as atualizações até 16-08-23 12:53

---
+++ project0001/scripts/script.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll('section');
+sections.forEach((section) => {
+    section.addEventListener('click', () => {
+        // alert('clicou');
+        section.style.backgroundColor = 'rgba(34, 34, 34, 0.774)'
+        section.style.fontWeight = '800'
+
+    });
+});
+
+const logoHome = document.getElementById('logo');
+
+function backHome() {
+    logoHome.addEventListener('click', () => {
+        window.location = '../index.html';
+    });
+}
+
+backHome();
+
+
+const sobre = document.getElementById('sobre');
+
+function goAbout() {
+    sobre.addEventListener('click', () => {
+        window.location = './html/about.html';
+    });
+}
+
+goAbout();
